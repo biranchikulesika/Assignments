74bfaa8c26b37bacdd6e4fa0d43530ec660022f9
biranchikulesika <biranchikulesika@gmail.com>
2024-11-22 11:11:23 +0530

    Create A6_Q2_floyd_triangle.c

---
+++ 1st_Year/LAB/Assignment_06/A6_Q2_floyd_triangle.c
@@ -0,0 +1,31 @@
+/*
+ *This r program is to print a Floyd's triangle.
+ *We will the right-angled isosceles triangle with consecutive numbers.
+ *
+ *Author: Biranchi Kulesika
+ *Date:
+ *Assignment No.: 06
+ *Question No.: 02
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int rows, r, c, number = 1;
+
+    printf("Enter the number of rows for Floyd's Triangle: ");
+    scanf("%d", &rows);
+
+    for (r = 1; r <= rows; r++)
+    {
+        for (c = 1; c <= r; c++)
+        {
+            printf("%d ", number);
+            number++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
