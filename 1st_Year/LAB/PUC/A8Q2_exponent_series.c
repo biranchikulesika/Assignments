5118436b9121b4e4d8dcc11ee6c5737867faa991
biranchikulesika <biranchikulesika@gmail.com>
2024-12-02 22:00:02 +0530

    Created A8Q1_exponent_series.c

---
+++ 1st_Year/LAB/PUC/A8Q2_exponent_series.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <math.h>
+
+int getMaxTerms(int x)
+{
+    if (x <= 5)
+    {
+        return 32;
+    }
+    else if (x <= 15)
+    {
+        return 16;
+    }
+    else if (x < 35)
+    {
+        return 13;
+    }
+    else
+    {
+        return 9;
+    }
+}
+
+int main()
+{
+    long long sum = 0;
+    int n, x, max_terms;
+
+    printf("Enter the value of x (1-100): ");
+    scanf("%d", &x);
+
+    max_terms = getMaxTerms(x);
+
+    printf("Enter the number of terms (1-%d): ", max_terms);
+
+    int l = 1;
+    int count = 0;
+    while (l == 1)
+    {
+        scanf("%d", &n);
+
+        if (n <= max_terms && n >= 1)
+        {
+            l = 0;
+        }
+        else
+        {
+            if (count == 3)
+            {
+                printf("\nProgram terminated due to multiple wrong inputs.\n\n");
+                return 0;
+            }
+            printf("\nCannot calculate for %d terms due to risk of data overflow: ", n);
+            printf("\nPlease re-enter the number of terms within 1 - %d: ", max_terms);
+            count++;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += pow(x, i);
+    }
+
+    printf("\nThe sum of the series 1 + x + x^2 + x^3 + x^4 + ... up to %d terms is: %lld\n\n", n, sum);
+
+    printf("\n===Code Execution Successful====\n\n");
+
+    return 0;
+}
