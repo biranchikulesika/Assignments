a11e1a069649093f62bb22200bf0d8d9f426c43b
biranchikulesika <biranchikulesika@gmail.com>
2024-12-12 17:07:37 +0530

    Created but changes required.

---
+++ 1st_Year/LAB/Python/Assignmnent_03/A3Q4_reverse_string_2.py
@@ -0,0 +1,19 @@
+"""
+* Assignment No.: 03
+* Question No.: 04
+* Date: [empty]
+
+* Program Name: Reverse a String 2
+* Description: This python program takes a string as run-time input
+* and reverses and prints it
+
+* Author: Biranchi Kulesika
+* Date: 10 Dec, 2024
+"""
+#Incomplete Program
+#Incomplete Program
+
+string = input("\nEnter a sentence to reverse it: ")
+print(f"\nThe entered string is: {string}")
+reverse = string[::-1]
+print(f"\nThe reversed string is: {reverse}\n")
