16a529a0527f4ef7a0757e24e6c90eb67a0d8a39
biranchikulesika <biranchikulesika@gmail.com>
2025-07-14 20:22:08 +0530

    Add program to convert a list of elements into a string

---
+++ 1st_Year/1SEM/Python/Assignment_04/a4q7_conversion.py
@@ -0,0 +1,16 @@
+"""
+* Assignment No.: 04
+* Question No.: 07
+* Date: 13 Nov, 2024
+
+* Description: This python program converts a list of elements into a string
+
+* Author: Biranchi Kulesika
+* Date: 14 July, 2025
+"""
+
+letters = input("Enter letters separated by space: ").split()
+
+result_string = "".join(letters)
+
+print("Converted string: ", result_string)
